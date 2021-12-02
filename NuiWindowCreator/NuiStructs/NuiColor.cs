bd2bb592947197357bcd644337d3d762a6275072
Алексей <stenly87@live.ru>
2021-12-02 22:17:47 +1000

    color

--- NuiWindowCreator/NuiStructs/NuiColor.cs
+++ NuiWindowCreator/NuiStructs/NuiColor.cs
@@ -2,9 +2,22 @@
 {
     public class NuiColor : NuiStruct
     {
-        public byte a;
-        public byte r;
-        public byte g;
-        public byte b;
+        public byte a { get; set; }
+        public byte r { get; set; }
+        public byte g { get; set; }
+        public byte b { get; set; }
+
+        public NuiColor(byte a, byte r, byte g, byte b)
+        {
+            this.a = a;
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        public NuiColor()
+        {
+            a = r = g = b = 255;
+        }
     }
 }
